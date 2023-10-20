bc0a69bafb9e451726f3cd9e51fe1ad3d32c4b39
MATHEWMESFIN <mathewmesfin@gmail.com>
2023-10-19 21:13:31 -0400

    added database

---
+++ src/app/User.jsx
@@ -0,0 +1,24 @@
+async function getUsers() {
+    const res = await fetch('http://localhost:4000/users', {
+        next: {
+            revalidate: 0
+        }
+    })
+
+    return res.json()
+}
+export default async function Users() {
+    const users = await getUsers()
+
+    return (
+      <div>
+        {users.map((user) => (
+            <div key={user.id}>
+                <h1>{user.name}</h1>
+                <p>{user.password}</p>
+            </div>
+            )
+        )}
+      </div>
+    )
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,10 @@
 import FlightSearch from "./components/FlightSearch"
+import Users from "./user"
 
 export default function Home() {
   return (
     <main>
+      <Users />
       <h2>Dashboard</h2>
       <FlightSearch />
     </main>
