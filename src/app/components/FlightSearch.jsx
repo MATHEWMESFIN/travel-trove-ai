8cad8cc10f69dc0c41451b04ca3150240804de08
MATHEWMESFIN <mathewmesfin@gmail.com>
2023-11-26 18:42:20 -0500

    added ai chat feature

--- src/app/components/FlightSearch.jsx
+++ src/app/components/FlightSearch.jsx
@@ -1,7 +1,7 @@
 "use client"
 import '../style/FlightSearch.css'
 import React, { useState } from 'react';
-import { useSession } from 'next-auth/react'
+import Chat from './Chat'
 
 export default function FlightSearch() {
 
@@ -17,11 +17,12 @@ export default function FlightSearch() {
     const [maxPrice, setMaxPrice] = useState('');
 
     return (
-        <div className="container">
+        <div className="search-container">
             <div>
                 <div className="row">
                     <div className="flight-search">Flight Search &#128747;</div>
                 </div>
+                <Chat />
                 <form className="flight-form">
                     <div className="row">
                         <div className="form-group">
