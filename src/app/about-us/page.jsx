8305d14b474e79622b1a10cea6448064b989072d
Chris Blackwell <christopherblackwell50@gmail.com>
2023-12-07 14:25:11 -0500

    Changes to About Us page

--- src/app/about-us/page.jsx
+++ src/app/about-us/page.jsx
@@ -1,7 +1,26 @@
 export default function AboutUs() {
   return (
     <main>
-      <h2>About Us</h2>
+      <h2>Welcome to TravelTroveAI - Your Personal AI Travel Companion! <br></br> <br></br>
+
+At TravelTroveAI, we're passionate about travel and technology. We've created a unique platform tailored for adventurous people who want to explore the world. Our app is more than just a travel planner; it's an intelligent companion that understands your travel style and preferences.
+<br></br><br></br>
+Personalized Travel Itineraries
+Using advanced AI algorithms, we curate travel itineraries that are unique to each user. Tell us about your interests, budget, and desired destinations, and let our app do the rest. Whether you're looking for a tranquil beach holiday, a cultural city break, or an adventurous trek, we've got you covered.
+<br></br><br></br>
+
+Real-Time Updates and Information
+Travel with confidence knowing that you have the latest information at your fingertips. Our app provides real-time weather updates to help you pack and plan accordingly. Exploring new places is easier with our instant language translation feature, breaking down language barriers as you travel.
+<br></br><br></br>
+Safety First
+Your safety is our priority. We offer up-to-date local safety information, ensuring you're aware of any travel advisories or important local customs. Explore the world with peace of mind.
+<br></br>
+Join Our Community
+Become part of a community of like-minded travelers. Share experiences, tips, and make connections. With TravelTroveAI, every trip is an opportunity for a new adventure.
+<br></br><br></br>
+Ready to embark on your next adventure? Visit TravelTroveAI today and start traveling!
+
+</h2>
     </main>
   )
 }
